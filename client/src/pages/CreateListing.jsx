67e012852823e09969e2f1c91e681bf577a0c99f
Droxinia <dagmawishimelis2@gmail.com>
2023-12-01 15:27:42 +0300

    complete create listing functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,14 +1,27 @@
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
-import { useState } from 'react'
+import { useState } from 'react';
 import { app } from '../firebase';
+import {useSelector} from 'react-redux';
+import {useNavigate} from 'react-router-dom';
 
 export default function CreateListing() {
+    const navigate = useNavigate();
+    const {currentUser} = useSelector(state => state.user)
     const [files, setFiles] = useState([]);
     const [formData, setFormData] = useState({
         imageUrls:[],
+        name:'',
+        description:'',
+        address:'',
+        price:1,
+        type:'',
+
+
     });
     const [ImageUploadError, setImageUploadError] = useState(false);
     const [uploading,setUpLoading] = useState(false);
+    const [error, setError] = useState(false);
+    const [loading, setLoading]= useState(false);
     console.log(formData);
     const handleImageSubmit = (e) => {
         if(files.length>0  && files.length + formData.imageUrls.length < 7){
@@ -24,11 +37,14 @@ export default function CreateListing() {
                 
 
             }).catch((err) => {
-                setImageUploadError('image upload failed(2, per image)');
+                setImageUploadError('image upload failed(2 mb, per image)');
                 setUpLoading(false);
-            })
+            })  .finally(() => {
+                setUpLoading(false); 
+              });
+            
 
-
+            
         }else{
             setImageUploadError('you can only upload 6 images per listing');
             setUpLoading(false);
@@ -69,26 +85,66 @@ export default function CreateListing() {
         })
     }
 
+  
+    const handleChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.id]:e.target.value
+        })
+    }
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+       if(formData.imageUrls.length<1) return setError('you must upload atleast one image');
+       setLoading(true);
+       setError(false);
+       const res = await fetch ('/api/listing/create', {
+        method: 'POST',
+        headers:{
+            'Content-Type' : 'application/json',
+        },
+        body: JSON.stringify({
+            ...formData,
+            userRef: currentUser._id,
+        }),
+       }) ;
+       const data = await res.json();
+       setLoading(false);
+       if(data.success ===false) {
+        setError(data.message);
+       }
+
+       
+      navigate(`/listing/${data._id}`) 
+    } catch (error) {
+       setError(error.message);
+       setLoading(false); 
+    }
+  }
   return (
    <main className='p-3 max-w-4xl mx-auto'>
     <h1 className='text-3xl font-semibold text-center my-7'>Create a Listing</h1>
-    <form className='flex flex-col sm:flex-row gap-4'>
+    <form onSubmit={handleSubmit} className='flex flex-col sm:flex-row gap-4'>
         <div className='flex flex-col gap-4 flex-1'>
         <select
         className='border p-3 rounded-lg'
         id='type'
         required
+        onChange={handleChange}
+        value={formData.type}
 >
-        <option value=''>Select Type</option>
+        <option value='' disabled>Select Type</option>
         <option value='Services'>Services</option>
         <option value='Product'>Product</option>
         </select>
 
-            <input type='text' placeholder='Name' className='border p-3 rounded-lg ' id='name' maxLength='62' minLength='10' required />
-            <textarea type='text' placeholder='Description' className='border p-3 rounded-lg ' id='description' maxLength='62' minLength='10' required />
-            <input type='text' placeholder='Address' className='border p-3 rounded-lg ' id='address'  required />
+            <input type='text' placeholder='Name' className='border p-3 rounded-lg ' id='name' maxLength='62' minLength='10' required onChange={handleChange} value={formData.name}/>
+            <textarea type='text' placeholder='Description' className='border p-3 rounded-lg ' id='description' maxLength='62' minLength='10' required onChange={handleChange} value={formData.description} />
+            <input type='text' placeholder='Address' className='border p-3 rounded-lg ' id='address'  required onChange={handleChange} value={formData.address} />
         <div className='flex items-center gap-2'>
-            <input type='number' id='Price' min='1' max='10' required className='p-3 border border-gray-300 rounded-lg'/>
+            <input type='number' id='price'  required className='p-3 border border-gray-300 rounded-lg' min='1' max='1000000' onChange={handleChange} value={formData.price} />
             <div><p>price</p></div>
         </div>
         </div>
@@ -99,7 +155,7 @@ export default function CreateListing() {
             </p>
             <div className='flex gap-4 '>
                 <input onChange={(e) => setFiles(e.target.files) } className='p-3 border border-gray-300 rounded w-full' type='file' id='images' accept='image/*' multiple />
-                <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border-green-500 rounded uppercase hover:shadow-lg disabled:opacity-80'>
+                <button type='button' onClick={handleImageSubmit} className='p-3 text-green-700 border-green-500 rounded uppercase hover:shadow-lg disabled:opacity-80' disabled={uploading}>
                     {uploading ? 'uploading...' : 'upload'}
                     </button>
             </div>
@@ -114,7 +170,10 @@ export default function CreateListing() {
             ))
         }
 
-         <button className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>Create the AD</button>
+         <button disabled={loading || uploading} className='p-3 bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 disabled:opacity-80'>
+            {loading ? 'Creating ...' : 'create listing'}
+            </button>
+            {error && <p className='text-red-700 text-sm'>{error}</p>}
        
         </div>
         
