0ac89392080fabb05f1faab95ce1be9265782c1d
Droxinia <dagmawishimelis2@gmail.com>
2023-12-06 14:02:17 +0300

    created an api route for listing update

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -14,15 +14,37 @@ export const deleteListing = async(req,res,next) => {
     const listing = await Listing.findById(req.params.id);
 
     if(!listing) {
-        return next(errorHandler(404, 'listing not found'))
+        return next(errorHandler(404, "listing not found"))
     }
 
     if(req.user.id !== listing.userRef) {
-        return next(errorHandler(404, 'You can only delete your own lisitng'))
+        return next(errorHandler(404, "You can only delete your own lisitng"))
     }
     try {
         await Listing.findByIdAndDelete(req.params.id);
         res.status(200).json('Lisitng has been deleted');
+    } catch (error) {
+        next(error);
+    }
+}
+
+export const updateListing= async (req, res, next) => {
+    const listing= await Listing.findById(req.params.id);
+
+    if(!listing){
+        return next(errorHandler(404, 'listing not found'));
+    }
+
+    if(req.user.id !== listing.userRef){
+        return next(errorHandler(401, 'you can only update your own lisitings!'));
+    }
+    try {
+        const updatedListing= await Listing.findByIdAndUpdate(
+            req.params.id,
+            req.body,
+            {new: true }
+        );
+      res.status(200).json(updatedListing);
     } catch (error) {
         next(error);
     }
