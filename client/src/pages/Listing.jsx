b0d612149978636e1db746257b3a67668b1d7535
Droxinia <dagmawishimelis2@gmail.com>
2023-12-14 08:08:28 +0300

    contact ad owner button for listing

--- client/src/pages/Listing.jsx
+++ client/src/pages/Listing.jsx
@@ -5,6 +5,8 @@ import SwiperCore from 'swiper';
 import {Navigation} from 'swiper/modules';
 import 'swiper/css/bundle';
 import { FaMapMarkerAlt, FaShare } from 'react-icons/fa';
+import {useSelector } from 'react-redux';
+import Contact from '../components/Contact';
 
 export default function Listing() {
     SwiperCore.use(Navigation)
@@ -12,7 +14,9 @@ export default function Listing() {
     const [listing, setListing] = useState(null);
     const [loading, setLoading] = useState(false);
     const[error, setError] = useState(false); 
-    const params = useParams()
+    const [contact, setContact] = useState(false);
+    const params = useParams();
+    const {currentUser} =useSelector((state) => state.user);
   useEffect(() =>{
     const fetchListing = async () => {
         try {
@@ -102,6 +106,14 @@ export default function Listing() {
             </span>
             {listing.description}
         </p>
+        {currentUser && listing.userRef !== currentUser._id && !contact && (
+            <button onClick={() =>setContact(true)} className='bg-slate-700 text-white rounded-lg uppercase hover:opacity-95 p-3'>
+            contact provider
+        </button>
+        )}
+        
+         {contact && <Contact listing={listing}/> }
+
 
         </div>
         
