12388aceef2b81fb512a488198a59bff0d07d712
Droxinia <dagmawishimelis2@gmail.com>
2023-12-16 07:54:52 +0300

    fixed syntax error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/home'
+import Home from './pages/Home.jsx'
 import About from './pages/about'
 import ContactUs from './pages/contactus'
 import SignUp from './pages/signup'
