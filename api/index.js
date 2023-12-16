b7e9081b23e9c89d8335b666262140947a7c3c85
Droxinia <dagmawishimelis2@gmail.com>
2023-12-16 07:40:40 +0300

    deployment setup

--- api/index.js
+++ api/index.js
@@ -5,7 +5,8 @@ import userRouter from './routes/user.route.js';
 import authRouter from './routes/auth.route.js';
 import cors from 'cors'; 
 import cookieParser from 'cookie-parser';
-import listingRouter from './routes/listing.route.js'
+import listingRouter from './routes/listing.route.js';
+import path from 'path';
 
 dotenv.config();
 
@@ -14,6 +15,8 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
     console.log(err);
 });
+
+const _dirname = path.resolve();
 
 const app = express();
 app.use(express.json());
@@ -29,6 +32,11 @@ app.listen(3000, () => {
 app.use('/api/user', userRouter);
 app.use('/api/auth', authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(_dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
